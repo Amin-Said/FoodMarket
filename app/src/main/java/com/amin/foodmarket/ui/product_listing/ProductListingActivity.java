72bbec71211f517f1dc69babc8e6df2415aa5a11
Amin <Aminsaid_2011@hotmail.com>
2020-02-13 11:06:08 +0200

    solve the toolbar in product list activity bug with system navigation bar

--- app/src/main/java/com/amin/foodmarket/ui/product_listing/ProductListingActivity.java
+++ app/src/main/java/com/amin/foodmarket/ui/product_listing/ProductListingActivity.java
@@ -7,6 +7,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -29,6 +30,11 @@ public class ProductListingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         setContentView(R.layout.activity_product_listing);
 
